1d54045a47ad96949aea2ee20a6ea5caa33c8fa7
JpMeira <jp.mpmeira@gmail.com>
2024-10-01 20:45:15 -0300

    teste

--- meu_estoque_app/lib/models/categoria.dart
+++ meu_estoque_app/lib/models/categoria.dart
@@ -5,12 +5,12 @@ class Categoria {
   Categoria({this.id, required this.nome});
 
   factory Categoria.fromMap(Map<String, dynamic> json) => Categoria(
-    id: json['id_categoria'],
-    nome: json['nome'],
-  );
+        id: json['id_categoria'],
+        nome: json['nome'],
+      );
 
   Map<String, dynamic> toMap() => {
-    'id_categoria': id,
-    'nome': nome,
-  };
+        'id_categoria': id,
+        'nome': nome,
+      };
 }
